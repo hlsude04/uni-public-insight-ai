693a7a798236c947e30f6a3b8956d85cbb1a853d
hlsude04 <hlsude04@gmail.com>
2026-03-31 22:51:28 +0300

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useMemo, useState } from "react";
-import { loadCsvFromPublic } from "../lib/csv";
-import type { CsvRow } from "../lib/csv";
+import { loadCsvFromPublic } from "./lib/csv";
+import type { CsvRow } from "./lib/csv";
 import { Header } from "./components/Header";
 import { Dashboard } from "./components/Dashboard";
 import { GeminiPanel } from "./components/GeminiPanel";
